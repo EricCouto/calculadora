901346c60e8d6f86f455193c58f2c9d157780cb1
Eric Couto <ericcouto3@gmail.com>
2023-04-21 22:19:05 -0300

    fundo do visor, fonte e posicionamento da fonte no visor(display)

--- src/br/com/cod3r/calc/visao/Display.java
+++ src/br/com/cod3r/calc/visao/Display.java
@@ -1,11 +1,25 @@
 package br.com.cod3r.calc.visao;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Display extends JPanel {
-	public Display() {
 
+	private final JLabel label;
+
+	public Display() {
+		setBackground(new Color(46, 49, 50));
+
+		label = new JLabel("12345,67");
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("courier", Font.PLAIN, 20));
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+		add(label);
 	}
 
 }
